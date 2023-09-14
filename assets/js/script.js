800a2f28d2e430b37bd87747e971500fcf152dd0
Bishal Kc <bkc1326@gmail.com>
2023-09-14 12:40:01 +0545

    added js

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,4 +19,27 @@ scrollUpBtn.addEventListener("click", () => {
     });
 });
 
+// var scroll = window.pageYOffset;
+// var scrollup = document.querySelector('.scroll-up-btn');
+// console.log(scrollup);
+
+// // BACK TO TOP
+// scrollup.addEventListener('click', function() {
+//     scrollup.style.scrollBehavior = "smooth";
+//   window.scrollTo({
+//     top: 0,
+//     behavior: 'smooth'
+//   });
+
+// });
+
+// window.addEventListener('scroll', function() {
+//   var scroll = window.pageYOffset;
+
+//   if (scroll >= 200) {
+//     scrollup.style.display = 'block';
+//   } else {
+//     scrollup.style.display = 'none';
+//   }
+// });
 
