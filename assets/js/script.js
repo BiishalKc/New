1b5ca7456934e8db9f5865e9e7a43ed0a68d8657
Bishal Kc <bkc1326@gmail.com>
2023-09-29 10:11:08 +0545

    modified responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,34 +12,3 @@ function checkHeight(){
     }
 }
 
-scrollUpBtn.addEventListener("click", () => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-});
-
-// var scroll = window.pageYOffset;
-// var scrollup = document.querySelector('.scroll-up-btn');
-// console.log(scrollup);
-
-// // BACK TO TOP
-// scrollup.addEventListener('click', function() {
-//     scrollup.style.scrollBehavior = "smooth";
-//   window.scrollTo({
-//     top: 0,
-//     behavior: 'smooth'
-//   });
-
-// });
-
-// window.addEventListener('scroll', function() {
-//   var scroll = window.pageYOffset;
-
-//   if (scroll >= 200) {
-//     scrollup.style.display = 'block';
-//   } else {
-//     scrollup.style.display = 'none';
-//   }
-// });
-
